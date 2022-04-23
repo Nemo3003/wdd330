46f3b6b50e4c3741e9a8ab3351f7f66b20b115bb
Nemo3003 <ulimarian2000@gmail.com>
2022-04-23 12:33:27 -0300

    chapter3

--- week2/practice.js
+++ week2/practice.js
@@ -1,6 +1,16 @@
 "use strict";
-a = 10;
+let a = 10;
 console.log(a);
 let $dollars = 10;
 let _hollycow = 10;
 let $ = 10;
+//---------------------------------------------
+const Numerals = new Map();
+Numerals.set(1, 'I');
+Numerals.set(2, 'II');
+Numerals.set(3, 'III');
+Numerals.get(3);
+Numerals.has(10);
+
+let newNumerals = [...Numerals];
+console.log(newNumerals);
