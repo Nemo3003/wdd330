566c6cd7bae9d5c8d4f84d28f0505c6490a0f643
Nemo3003 <ulimarian2000@gmail.com>
2022-04-24 08:56:28 -0300

    'functions'

--- week2/practice.js
+++ week2/practice.js
@@ -14,3 +14,22 @@ Numerals.has(10);
 
 let newNumerals = [...Numerals];
 console.log(newNumerals);
+//----------------------------------------------
+function mean(...values) {
+    let total = 0;
+    for(const value of values) {
+    total += value;
+    }
+    return total/values.length;
+    }
+console.log(mean(2,3,4,5,6,7))
+//---------------------------------------------
+let bro = "How you remind me"
+function Ending(func){
+    console.log(`of who I really am`);
+}
+const Sing = (song,callback) => {
+    console.log(`I'm singing along to ${song}.`);
+    callback();
+    }
+Sing(bro, Ending);
