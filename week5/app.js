60178d6607f4ff5d644d5044fad415887ea6f325
Nemo3003 <ulimarian2000@gmail.com>
2022-05-15 08:22:00 -0300

    Week 5

---
+++ week5/app.js
@@ -0,0 +1,18 @@
+'use strict';
+//Just a comment
+console.log('start() invoked');
+console.log('ask() invoked');
+console.log('check(event) invoked');
+console.log('gameOver() invoked');
+//Some testing code using Console.log
+console.log('Hello World');
+//Example using try catch finally
+try {
+    console.log('try block');
+    throw new Error('error');}
+catch (e) {
+    console.log('catch block');
+}
+finally {
+    console.log('finally block');
+}
