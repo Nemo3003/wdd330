758e103289c736e2f9179f2fb06923f48f3798c6
Nemo3003 <ulimarian2000@gmail.com>
2022-06-21 16:52:06 -0300

    nav and footer

--- Final/js/app.js
+++ Final/js/app.js
@@ -1,1 +1,28 @@
 "use strict";
+
+document.getElementById("modify").innerHTML = document.lastModified
+document.getElementById("year").innerHTML = new Date().getFullYear()
+
+let numOfVisits = document.querySelector('.daysOfVisit');
+
+let numOfVisits1 = Number(window.localStorage.getItem('visits'));
+let lastVisits= Number(window.localStorage.getItem('lastVisits'));
+
+const NUMS = 1000 * 60 * 60 * 24;
+
+let daysAmong = Date.now() - lastVisits;
+
+let numOfDays = Math.ceil(daysAmong / NUMS);
+
+localStorage.setItem('lastVisits', Date.now());
+
+if (numOfVisits1 != 0) {
+
+    numOfVisits.textContent = 'It\'s been ' + numOfDays + ' day(s) since your last visit.'
+} else {
+    numOfVisits.textContent = 'This is your first page visit.'
+}
+
+numOfVisits1++;
+localStorage.setItem("visits", numOfVisits1);
+
