033bc3a8c266e896daa1579735ec32404543f09f
Nemo3003 <ulimarian2000@gmail.com>
2022-06-30 17:01:21 -0300

    getting mad

--- Final/js/ricky.js
+++ Final/js/ricky.js
@@ -10,7 +10,40 @@ const Cards = (newData) =>{
         let numberCounter = 0;
         newData.forEach(character =>{
             let ids = character.id;
-            cardWrapper.innerHTML = cardWrapper.innerHTML + 
+            let div = document.createElement("div");
+            let name = document.createElement("h2");//name
+            let par = document.createElement("h4");//species
+            let par2 = document.createElement("h4");//origin
+            let par3 = document.createElement("h4");//status
+            let img = document.createElement("img");//image
+            let buttonLike = document.createElement("button");//button
+            let spanLike = document.createElement("span");//spanLike
+            //Assigned values to the elements
+            name.innerHTML = character.name;
+            par.innerHTML = character.species;
+            par2.innerHTML = character.origin.name;
+            img.src = character.image;
+            spanLike.innerHTML = "Like";
+            buttonLike.appendChild(spanLike);
+            //Assigned classes to the elements
+            div.classList.add("card-char");
+            name.classList.add("name");
+            par.classList.add("species");
+            par2.classList.add("origin");
+            par3.classList.add("status");
+            img.classList.add("image");
+            buttonLike.classList.add("buttonLike");
+            //Appended the elements to the div
+            div.appendChild(name);
+            div.appendChild(par);
+            div.appendChild(par2);
+            div.appendChild(img);
+            div.appendChild(buttonLike);
+            //Appended the div to the cardWrapper
+
+            cardWrapper.appendChild(div);
+            
+            /*cardWrapper.innerHTML = cardWrapper.innerHTML + 
             `<div class="card-char">
             <h2>${character.name}<h2>
             <h4>Status: ${character.status}</h4>
@@ -18,29 +51,13 @@ const Cards = (newData) =>{
             <h4>Origin: ${character.origin.name}</h4>
             <img src=${character.image} class="char-img">
             <br>
-            <button class="like" type="button">Like</button>
-            <span id="likes">${numberCounter}</span>
+            <button class="like${character.id}" type="button">Like</button>
+            <span id="likes${character.id}">${numberCounter}</span>
             </div>
             <br>
-            `;
+            `;*/
             
             },
         )
        
-        //Now using localstorage to store the number of likes each character has
-        const likeButtons = document.querySelectorAll(".like");
-        likeButtons.forEach(button =>{
-            button.addEventListener("click", () =>{
-                let likes = button.nextElementSibling;
-                let number = parseInt(likes.innerHTML);
-                number++;
-                likes.innerHTML = number;
-                localStorage.setItem(button.parentElement.id, number);
-            });
-            //display the current number of likes each character has
-            let currentLikes = localStorage.getItem(button.parentElement.id);
-            if(currentLikes){
-                button.nextElementSibling.innerHTML = currentLikes;
-            }
-        }
-        );}
+    }
