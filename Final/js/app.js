30889f4b09f3dbb64b63d428411c0eae02bcdbc7
Nemo3003 <ulimarian2000@gmail.com>
2022-07-05 17:23:01 -0300

    Final, completed'

--- Final/js/app.js
+++ Final/js/app.js
@@ -26,3 +26,23 @@ if (numOfVisits1 != 0) {
 numOfVisits1++;
 localStorage.setItem("visits", numOfVisits1);
 
+let hamb = document.getElementById("hamburger")
+let nav = document.getElementById("nav")
+//when the page resizes to a smaller size, the nav bar will disappear and the hamburger will appear
+window.addEventListener('resize', function () {
+    if (window.innerWidth < 768) {
+        hamb.style.display = "block";
+    } else {
+        hamb.style.display = "none";}
+    if(this.window.innerWidth < 768) {
+        nav.style.display = "none";}});
+//When the user clicks on the hamburger, then the nav will show up
+hamb.addEventListener('click', function () {
+    nav.style.display = "block";
+    hamb.style.display = "none";    
+});
+//if you click on the hamburger again, it will disappear immediately
+hamb.removeEventListener('click', function () {
+    nav.style.display = "none";
+    hamb.style.display = "block";
+});
